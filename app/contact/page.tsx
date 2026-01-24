f957b2f97825d15c81fe606905c6f93356dfe34f
slipperystick3r <kyle7tran@gmail.com>
2026-01-23 18:01:55 -0800

    Align icons on top of vertical line and align text in the middle of rectangular section.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -60,16 +60,16 @@ export default function Example() {
 							<Link
 								href={s.href}
 								target="_blank"
-								className="p-4 relative flex flex-col items-center justify-center gap-4 duration-700 group md:gap-8 h-full min-h-[300px] md:min-h-[400px] lg:min-h-[500px]"
+								className="p-4 relative flex flex-col items-center duration-700 group h-full min-h-[300px] md:min-h-[400px] lg:min-h-[500px]"
 							>
 								<span
-									className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
+									className="absolute w-px h-2/3 top-12 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
 									aria-hidden="true"
 								/>
-								<span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
+								<span className="relative z-10 mt-4 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
 									{s.icon}
-								</span>{" "}
-								<div className="z-10 flex flex-col items-center text-center">
+								</span>
+								<div className="z-10 flex flex-col items-center justify-center flex-1 text-center">
 									<span className="lg:text-xl font-medium duration-150 xl:text-3xl text-zinc-200 group-hover:text-white font-display text-center">
 										{s.handle}
 									</span>
